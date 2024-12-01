5b3bd9599448e2493e145515c24bf8d0f04bdfbc
abc <abc@example.com>
2024-12-01 17:21:28 +0200

    pagaidām viss

--- pamats.R
+++ pamats.R
@@ -126,6 +126,7 @@ print(wilcox_test_result)
 
 wilcox_effsize(data=cukur,Tonn_Hect~DistrictPosition,paired=FALSE,ci=TRUE) #efekta apjoms ir 0.142, kas ir vājš efekts
 
+
 #Vizualizācija:
 ggplot(cukur, aes(x = DistrictPosition, y = Tonn_Hect, fill = DistrictPosition)) +
   geom_boxplot() +
@@ -526,35 +527,3 @@ dunn_test(Tonn_Hect ~ SoilName, data = cukur)
 
 
 
-#korelācija (nesanāk)
-
-p1 <- ggplot(cukur, aes(sample = HarvestMonth)) + geom_qq_band() +
-  stat_qq_point() + stat_qq_line() + labs(title = "Ievākšanas mēnesis")
-p2 <- ggplot(cukur, aes(sample = Age)) + geom_qq_band() +
-  stat_qq_point() + stat_qq_line() + labs(title = "vecums")
-p3 <- ggplot(cukur, aes(sample = Area)) + geom_qq_band() +
-  stat_qq_point() + stat_qq_line() + labs(title = "platiba")
-p4 <- ggplot(cukur, aes(sample = Tonn_Hect)) + geom_qq_band() +
-  stat_qq_point() + stat_qq_line() + labs(title = "tonnas")
-plot_grid(p1, p2, p3, p4)
-
-p5 <- ggplot(cukur, aes(sample = HarvestDuration)) + geom_qq_band() +
-  stat_qq_point() + stat_qq_line() + labs(title = "Ievākšanas ilgums")
-p6 <- ggplot(cukur, aes(sample = Fibre)) + geom_qq_band() +
-  stat_qq_point() + stat_qq_line() + labs(title = "Šķiedras")
-p7 <- ggplot(cukur, aes(sample = Sugar)) + geom_qq_band() +
-  stat_qq_point() + stat_qq_line() + labs(title = "Cukurs")
-
-plot_grid(p5, p6, p7)
-
-
-rcor.test(cukur[,8:10])
-
-cor.test(cukur$Tonn_Hect,cukur$Sugar)
-
-#regresijas analīze
-modelis <- lm(formula = Tonn_Hect ~  Age + Fibre + Sugar + HarvestMonth, data = cukur)
-summary(modelis)
-
-modelis1 <- lm(formula = Tonn_Hect ~  jul_96, data = cukur)
-summary(modelis1)
